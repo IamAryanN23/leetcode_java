f2e738f436869d36e4aeeedee63f8ac9d6b9af65
Aryan <apnarwankar23@gmail.com>
2023-12-02 23:51:49 +0530

    adding two more leetcode solutions

--- longest_common_prefix.java
+++ longest_common_prefix.java
@@ -1,11 +1,13 @@
 import java.util.Arrays;
 class longestCommonPrefix {
-    public static String longestCommon(String[] strs) {
+    public static String longestCommon(String[] strs) 
+    {
         Arrays.sort(strs);
         String s1 = strs[0];
         String s2 = strs[strs.length-1];
+        int len1=s1.length(), len2=s2.length();
         int idx = 0;
-        while(idx < s1.length() && idx < s2.length()){
+        while(idx < len1 && idx < len2){
             if(s1.charAt(idx) == s2.charAt(idx)){
                 idx++;
             } else {
@@ -15,7 +17,7 @@ class longestCommonPrefix {
         return s1.substring(0, idx);
     }
     public static void main(String[] args) {
-        String[] s1 = {"flower","flow","flight"};
+        String[] s1 = {"dog","racecar","car"};
         System.out.println(longestCommon(s1));
     }
 }
