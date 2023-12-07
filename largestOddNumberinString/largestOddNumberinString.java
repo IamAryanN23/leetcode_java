8437991fd738140073fda918c0f31f306fcf837b
Aryan N <apnarwankar23@gmail.com>
2023-12-07 17:07:12 +0530

    cleaned logic to accept any number

--- largestOddNumberinString/largestOddNumberinString.java
+++ largestOddNumberinString/largestOddNumberinString.java
@@ -1,19 +1,16 @@
 package largestOddNumberinString;
-
-public class largestOddNumberinString {
-
-    
-    public static String largestOddNumber(String num) {
-        int number = Integer.parseInt(num);
+public class largestOddNumberinString 
+{
+    public static String largestOddNumber(String num) 
+    {
         int length = num.length()-1;
-        if (number % 2 == 1) 
+        if ((int)num.charAt(num.length()-1)%2==1) 
         {
             return num;
         }
         while (length>=0) 
         {
-            int n = num.charAt(length);
-            if(n%2==1)
+            if(num.charAt(length)%2==1)
             {
                 return num.substring(0, length+1); 
             }
@@ -25,7 +22,7 @@ public class largestOddNumberinString {
 
     public static void main(String[] args) 
     {
-        String s = "35427";
+        String s = "7542351164447";
         System.out.println(largestOddNumber(s));
     }
 }
